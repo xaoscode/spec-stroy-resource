de8d0aa7c41c91e12fbae7d61aa68c2ae1190ec6
Nikita Buryak <laimongrace@gmail.com>
2025-01-26 17:29:22 +1000

    update

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "standalone",
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "3002",
+				pathname: "/uploads/**",
+			},
+			{
+				protocol: "http",
+				hostname: "spec-stroy-resource-api",
+				port: "3001",
+				pathname: "/uploads/**",
+			},
+		],
+	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/i,
@@ -19,17 +35,6 @@ const nextConfig: NextConfig = {
 				},
 			},
 		},
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "spec-stroy-resource-api:3001",
-				port: "",
-				pathname: "/uploads/images**",
-				search: "",
-			},
-		],
 	},
 };
 
