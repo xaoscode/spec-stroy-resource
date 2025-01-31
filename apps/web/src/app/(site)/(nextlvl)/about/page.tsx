b531c55ddfba7d51723838202453fa8a883623ea
Nikita Buryak <laimongrace@gmail.com>
2025-01-31 19:27:10 +1000

    g

--- apps/web/src/app/(site)/(nextlvl)/about/page.tsx
+++ apps/web/src/app/(site)/(nextlvl)/about/page.tsx
@@ -1,12 +1,27 @@
-
+import { IPage } from "@repo/interfaces"
+import { ContentRender } from "../services/[service]/components/ContentRedner/ContentRender"
+import { fetchPage } from "../../api/Pages"
 import { Metadata } from "next"
 import allMetadata from "../lib/metadata"
-import PageRender from "../components/PageRender/PageRender"
 
 export const metadata: Metadata = { ...allMetadata.about }
 
+interface IPageWithStatus extends IPage {
+  success: boolean
+}
 export default async function AboutPage() {
-  return <>
-    <PageRender></PageRender>
-  </>
+  const data: IPageWithStatus = await fetchPage('about')
+  return <div className="flex flex-col">
+
+    { data.success ? data.section.map((section) => (
+      <div key={ section.id } className="flex flex-col space-y-6 p-6 bg-gray-50 rounded-lg shadow-md gap-5">
+        <h1 className=" text-center">{ section.title }</h1>
+        { section.content.map((content) => (
+          <ContentRender key={ content.id } customContent={ content } />
+        )) }
+      </div>
+    )) : <div>Элементы страницы не найдены</div> }
+
+
+  </div>
 }
