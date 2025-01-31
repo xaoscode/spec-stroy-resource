a87e7d68c19691836563af58b8eac40357396c4b
Nikita Buryak <laimongrace@gmail.com>
2025-01-31 19:26:19 +1000

    g

--- apps/web/src/app/(site)/api/Pages.ts
+++ apps/web/src/app/(site)/api/Pages.ts
@@ -2,14 +2,19 @@ import { API } from "../../api";
 
 export async function fetchPage(slug: string) {
 	try {
-		const response = await fetch(`${API.pages}/get/${slug}`, { cache: "no-cache" });
+		if (!API.base) {
+			return { success: false };
+		}
+		const response = await fetch(`${API.pages}/get/${slug}`, {
+			method: "GET",
+			cache: "no-cache",
+		});
 		if (!response.ok) {
-			throw new Error(`Ошибка загрузки страницы: ${response.statusText}`);
+			throw new Error(`Ошибка загрузки проектов: ${response.statusText}`);
 		}
 		const data = await response.json();
 		return { ...data, success: true };
 	} catch (error) {
-		console.log(error);
-		return { success: false };
+		return { success: false, error: error };
 	}
 }
